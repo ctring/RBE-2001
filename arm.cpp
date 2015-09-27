3251f665209a9627e68300406e81791394b1660b
Cuong Nguyen <nguyentricuong1995@gmail.com>
2015-09-27 19:53:09 -0400

    Added arm library

---
+++ arm.cpp
@@ -0,0 +1,69 @@
+/**
+ * arms.h - Source code for controlling the arm
+ *
+ * Created on Sept 27, 2015 by Cuong Nguyen
+ */
+
+#include <Arduino.h>
+#include <Servo.h>
+
+#include "arm.h"
+#include "sensors.h"
+
+Servo gripper;
+Servo crane;
+Servo slider;
+
+void setupArm() {
+	slider.attach(SLIDER_PIN);
+	gripper.attach(GRIPPER_PIN);
+	crane.attach(CRANE_PIN);
+}
+
+int craneToHor() {
+	if (checkLimitSwitch(LIMIT_CRANE_HOR_PIN)) {
+		crane.write(CRANE_STOP);
+		return true;
+	}
+	crane.write(CRANE_TO_HOR);
+	return false;
+}
+
+int craneToVer() {
+	if (checkLimitSwitch(LIMIT_CRANE_VER_PIN)) {
+		crane.write(CRANE_STOP);
+		return true;
+	}
+	crane.write(CRANE_TO_VER);
+	return false;
+}
+
+int slideIn() {
+	if (checkLimitSwitch(LIMIT_SLIDE_IN_PIN)) {
+		slider.write(SLIDER_STOP);
+		return true;
+	}
+	slider.write(SLIDER_IN);
+	return false;
+}
+
+int slideOut() {
+	if (checkLimitSwitch(LIMIT_SLIDE_OUT_PIN)) {
+		slider.write(SLIDER_STOP);
+		return true;
+	}
+	slider.write(SLIDER_OUT);
+	return false;
+}
+
+int openGripper() {
+	gripper.write(GRIPPER_OPEN);
+	delay(1000);
+	return true;
+}
+
+int closeGripper() {
+	gripper.write(GRIPPER_CLOSE);
+	delay(1000);
+	return true;
+}
---
+++ arm.h
@@ -0,0 +1,33 @@
+/**
+ * arms.h - Header containing prototypes and macros for arms.cpp
+ *
+ * Created on Sept 27, 2015 by Cuong Nguyen
+ */
+#ifndef ARM_H
+#define ARM_H
+
+#define CRANE_PIN
+#define SLIDER_PIN 
+#define GRIPPER_PIN
+
+#define CRANE_TO_HOR 0
+#define CRANE_TO_VER 180
+#define CRANE_STOP 90
+
+#define SLIDER_IN 0
+#define SLIDER_OUT 180
+#define SLIDER_STOP 90
+
+#define GRIPPER_OPEN 180
+#define GRIPPER_CLOSE 0
+
+void setupArm();
+
+int craneToHor();
+int craneToVer();
+int slideIn();
+int slideOut();
+int openGripper();
+int closeGripper();
+
+#endif
