a248385bb2110f836b08b00d804e09420017a1a2
Cuong Nguyen <nguyentricuong1995@gmail.com>
2015-09-28 02:23:39 -0400

    Set up state controlling mechanism

--- final.ino
+++ final.ino
@@ -3,12 +3,38 @@
 #include "sensors.h"
 #include "movement.h"
 #include "arm.h"
+#include "states.h"	
 
 void setup() {
-  
+	setupMotors();
+	setupSensors();
+	setupArm();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int state = 0;
+int side = 0;
 
+void loop() {
+	bool finished = false;
+	switch (state) {
+		case 0: 
+			finished = getOldRod();
+			break;
+		case 1:
+			finished = storeOldRod();
+			break;
+		case 2: 
+			finished = getNewRod();
+			break;
+		case 3:
+			finished = placeNewRod();
+			break;
+	}
+	if (finished) {
+		state++;
+		if (state >= 4) {
+			state = 0;
+			side = 1 - side;
+		}
+	}	
 }
---
+++ states.h
@@ -0,0 +1,11 @@
+#ifndef STATES_H
+#define STATES_H
+
+int getOldRod();
+int storeOldRod();
+int getNewRod();
+int placeNewRod();
+
+
+
+#endif
