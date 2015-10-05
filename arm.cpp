3fbb4c9cf9330379377d635d4613c1de32b90463
Cuong Nguyen <nguyentricuong1995@gmail.com>
2015-10-05 03:53:18 -0400

    Added documentation

--- arm.cpp
+++ arm.cpp
@@ -10,12 +10,19 @@ Servo gripper;
 Servo crane;
 Servo slider;
 
+/**
+ * Sets up pins for slider, gripper and crane
+ */
 void setupArm() {
 	slider.attach(SLIDER_PIN);
 	gripper.attach(GRIPPER_PIN);
 	crane.attach(CRANE_PIN);
 }
 
+/**
+ * Rotates the crane to horizontal position.
+ * @return true when limit switch reached, false otherwise.
+ */
 int craneToHor() {
 	if (checkLimitSwitch(LIMIT_CRANE_HOR_PIN)) {
 		crane.write(CRANE_STOP);
@@ -25,6 +32,10 @@ int craneToHor() {
 	return false;
 }
 
+/**
+ * Rotates the crane to vertical position.
+ * @return true when limit switch reached, false otherwise.
+ */
 int craneToVer() {
 	if (checkLimitSwitch(LIMIT_CRANE_VER_PIN)) {
 		crane.write(CRANE_STOP);
@@ -34,6 +45,10 @@ int craneToVer() {
 	return false;
 }
 
+/**
+ * Slides the slider in.
+ * @return true when limit switch reached, false otherwise.
+ */
 int slideIn() {
 	if (checkLimitSwitch(LIMIT_SLIDE_IN_PIN)) {
 		slider.write(SLIDER_STOP);
@@ -43,6 +58,10 @@ int slideIn() {
 	return false;
 }
 
+/**
+ * Slides the slider out.
+ * @return true when limit switch reached, false otherwise.
+ */
 int slideOut() {
 	if (checkLimitSwitch(LIMIT_SLIDE_OUT_PIN)) {
 		slider.write(SLIDER_STOP);
@@ -52,12 +71,20 @@ int slideOut() {
 	return false;
 }
 
+/**
+ * Opens the gripper.
+ * @return true.
+ */
 int openGripper() {
 	gripper.write(GRIPPER_OPEN);
 	delay(1000);
 	return true;
 }
 
+/**
+ * Closes the gripper.
+ * @return true.
+ */
 int closeGripper() {
 	gripper.write(GRIPPER_CLOSE);
 	delay(1000);
